879ff5050cf54898e459afe7ef22a56a00456349
julia <julia@I-LOVE-BONO>
2025-04-22 13:39:47 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System_zarzadzania_produkcja_paczek_w_fabryce_logistycznej;
+
+namespace System_zarzadzania_produkcja_paczek_w_fabryce_logistycznej
+{
+    public interface IPaczka
+    {
+        void Przygotuj();
+    }
+
+
+    class MalaPaczka : IPaczka
+    {
+        public void Przygotuj()
+        {
+            Console.WriteLine("Przygotowano małą paczkę.");
+        }
+    }
+
+    class DuzaPaczka : IPaczka
+    {
+        public void Przygotuj()
+        {
+            Console.WriteLine("Przygotowano dużą paczkę.");
+        }
+    }
+
+    public interface IFabrykaPaczek
+    {
+        IPaczka UtworzPaczke();
+    }
+
+    public class FabrykaMalychPaczek : IFabrykaPaczek
+    {
+        public IPaczka UtworzPaczke()
+        {
+            return new MalaPaczka();
+        }
+    }
+
+    public class FabrykaDuzychPaczek : IFabrykaPaczek
+    {
+        public IPaczka UtworzPaczke()
+        {
+            return new DuzaPaczka();
+        }
+    }
+    public class ZarzadzaniePaczkami
+    {
+        private IFabrykaPaczek fabrykaPaczek;
+        private static ZarzadzaniePaczkami _instancja;
+        private ZarzadzaniePaczkami() {}
+        public static ZarzadzaniePaczkami Instancja
+        {
+            get
+            {
+                if (_instancja == null)
+                {
+                    _instancja = new ZarzadzaniePaczkami();
+                }
+
+                return _instancja;
+            }
+        }
+
+        public void UstawFabryke(IFabrykaPaczek fabryka)
+        {
+            fabrykaPaczek = fabryka;
+        }
+
+        public void ZrealizujZamowienie()
+        {
+            var paczka = fabrykaPaczek.UtworzPaczke();
+            paczka.Przygotuj();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var manager = ZarzadzaniePaczkami.Instancja;
+            manager.UstawFabryke(new FabrykaMalychPaczek());
+            manager.ZrealizujZamowienie();
+            manager.UstawFabryke(new FabrykaDuzychPaczek());
+            manager.ZrealizujZamowienie();
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("System obsługi produkcji paczek w fabryce logistycznej")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("System obsługi produkcji paczek w fabryce logistycznej")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("f32ae9a9-e31d-419c-a8fc-ac21e2edc05c")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
